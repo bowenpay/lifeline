60717a1ec3f5b32927085f770179e3f86ca1e764
yijingping <jingping.yi@gmail.com>
2016-03-11 15:17:36 +0800

    add Endings

---
+++ src/AllStates/StartUpEndings.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author 
+ *
+ */
+class StartUpEndings {
+    public static ENDINGS = [
+        { condition: { health: 80,wealth: 100,ability: 90,happiness: 90 },result: { title: "人生赢家",desc: "" } },
+        { condition: { health: 50,wealth: 50,ability: 50,happiness: 50 },result: { title: "平凡人生",desc: "" } },
+        { condition: { health: -100,wealth: -100,ability: -100,happiness: -100 },result: { title: "悲惨人生",desc: "" } }
+    ];  
+}
