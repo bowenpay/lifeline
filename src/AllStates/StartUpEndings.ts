35e72ad2f836d974cd08696936fb0117e56fc06f
SinCatGit <sincat@126.com>
2016-03-12 18:16:19 +0800

    调整皮肤样式，结果页不预加载

--- src/AllStates/StartUpEndings.ts
+++ src/AllStates/StartUpEndings.ts
@@ -11,6 +11,7 @@ class StartUpEndings {
                 title: "创业还没开始就已经结束",
                 desc: "        哈哈，显然你的这次尝试是非常失败的，你的创业还没有真正开始就已经结束了。全民创业，万众创新的口号不知道忽悠了多少人走上创业的路，但是创业其实是一件很难的事情，它需要资源，时机，运气和能力都具备才能成功。多提升自我，累计资源，攒好人品，重新来过吧。",
                 imageTag: "fail_tag_png",
+                weixintitle:"创业那些年，迈出了第一步",
                 imageBg: "fail_bg_png"
             } 
         },
@@ -18,7 +19,8 @@ class StartUpEndings {
             condition: { ending: 95 },
             result: {
                 title: "创业失败，但是收回了本钱",
-                desc: "        你已经很努力的将创业持续了两年多，但是俗话说的好，有的创业还没开始就已经失败了。看来你还需要继续努力，提高自己，尽量让下一次创业尝试走的更远。",
+                desc: "        你已经很努力的将创业持续了两年多，但是俗话说的好，有的创业还没开始就已经失败了。你得到了一定的回报，但你还需要继续努力，提高自己，尽量让下一次创业尝试走的更远。",
+                weixintitle:"创业那些年，赚到第一桶金",
                 imageTag: "fail_tag_png",
                 imageBg: "fail_bg_png"
             }
@@ -28,6 +30,7 @@ class StartUpEndings {
             result: { 
                 title: "身体还不够强健",
                 desc: "        创业需要有强健的体魄，在创业的过程中也需要注意锻炼身体。",
+                weixintitle:"创业那些年，累死累活拼搏",
                 imageTag: "fangqi_tag_png",
                 imageBg: "fangqi_bg_png"
             } 
@@ -37,6 +40,7 @@ class StartUpEndings {
             result: { 
                 title: "做了一个公司然后卖掉它",
                 desc: "        你在创业初期选择了退出创业，并得到了一笔不多不少的回报————比普通工作挣的多，但是考虑到你付出的时间和辛劳，你的回报也就没那么多了。坚持是创业的必备品质，你可能有些缺乏。",
+                weixintitle:"创业那些年，走向发家致富",
                 imageTag: "quit_tag_png",
                 imageBg: "quit_bg_png"
             } 
@@ -46,6 +50,7 @@ class StartUpEndings {
             result: { 
                 title: "公司被收购",
                 desc: "        能被收购其实也算是一种成功啦，在游戏设计中，只有4%的玩家能获得这个结局。你将创业进行到了后期，这本身已经是难得的成就。但是如果你某些问题上作出更棒的选择，说不定会有更好的结果...不论如何，还是祝贺你！",
+                weixintitle:"创业那些年，身家过亿了",
                 imageTag: "quit_tag_png",
                 imageBg: "quit_bg_png"
             } 
@@ -55,6 +60,7 @@ class StartUpEndings {
             result: { 
                 title: "公司被巨头收购",
                 desc: "        能被巨头收购也算是一种成功啦，在游戏设计中，只有3%的玩家能获得这个结局。你将创业进行到了后期，这本身已经是难得的成就。但是如果你再坚持一下，说不定会有更好的结果...不论如何，还是祝贺你！",
+                weixintitle:"创业那些年，身家过亿了",
                 imageTag: "quit_tag_png",
                 imageBg: "quit_bg_png"
             } 
@@ -64,6 +70,7 @@ class StartUpEndings {
             result: { 
                 title: "成功的创业者",
                 desc: "        根据游戏设计，只有3%的玩家能获得这个结局，你的公司做的很成功，虽然没有上市，但是无论在个人能力，个人财富，公司价值和成功度上，你已经超越了绝大多数人。创业是对综合实力的考验，也许你只是对一两个问题的选择欠妥，但他们影响了你最终的成绩。",
+                weixintitle:"创业那些年，走向人生巅峰",
                 imageTag: "sucess_tag_png",
                 imageBg: "quit_bg_png"
             } 
@@ -73,6 +80,7 @@ class StartUpEndings {
             result: { 
                 title: "成功在A股上市",
                 desc: "        根据游戏设计，只有不到1%的玩家能获得这个结局，充分体现了你强大的个人能力和对创业的理解。你具有坚韧的毅力和过人的胆识，另外，运气也站在你这边。祝贺你！欢迎关注我们的公众号，将您在创业路上遇到的问题发送给我们，帮助我们完善问题库，提高游戏的真实度。",
+                weixintitle:"创业那些年，在A股上市",
                 imageTag: "sucess_tag_png",
                 imageBg: "quit_bg_png"
             } 
@@ -82,6 +90,7 @@ class StartUpEndings {
             result: { 
                 title: "成功在纳斯达克上市",
                 desc: "        根据游戏设计，只有不到1%的玩家能获得这个结局，充分体现了你强大的个人能力和对创业的理解。你具有坚韧的毅力和过人的胆识，另外，运气也站在你这边。祝贺你！欢迎关注我们的公众号，将您在创业路上遇到的问题发送给我们，帮助我们完善问题库，提高游戏的真实度。",
+                weixintitle:"创业那些年，纳斯达克敲钟",
                 imageTag: "sucess_tag_png",
                 imageBg: "sucess_bg_png"
             } 
@@ -93,6 +102,7 @@ class StartUpEndings {
             result: {
                 title: "成功的企业家敢于知难而退",
                 desc: "        从某种意义上来说，你的人生已经称得上成功了，公司变卖后的剩余资产已足够你安享晚年。可能你梦想是成为马云，只不过在一些决策上失误了。Anyway，你已经在这个游戏里超过70%的玩家了！不信？那就请你的好友一起来试试吧~",
+                weixintitle:"创业那些年，实现成功人生",
                 imageTag: "quit_tag_png",
                 imageBg: "quit_bg_png"
             }
@@ -102,6 +112,7 @@ class StartUpEndings {
             result: {
                 title: "明哲保身的千万富翁",
                 desc: "        在互联网创业中期，你发现现实或许并不像你想的那样美好，知性的你选择停止进一步的尝试。卖掉了公司，你已然成为千万富翁，或许你的选择是正确的，不是每个人都能成为马云或比尔盖茨。虽壮志未酬，你仍然是人生的赢家！",
+                weixintitle:"创业那些年，成为千万富翁",
                 imageTag: "quit_tag_png",
                 imageBg: "quit_bg_png"
             }
@@ -111,6 +122,7 @@ class StartUpEndings {
             result: {
                 title: "不到黄河心不死",
                 desc: "        奋力拼搏的你并没有得到想要的结果，甚至搭上了自己的幸福和财富。尽管你心仍有不甘，建议你还是冷静下来思考下创业这些年所做过的决定，抓住转瞬即逝的机遇，从头再来一次。或许，成功就在下个路口等你！",
+                weixintitle:"创业那些年，奋斗至最后",
                 imageTag: "fail_tag_png",
                 imageBg: "fail_bg_png"
             }
@@ -120,6 +132,7 @@ class StartUpEndings {
             result: {
                 title: "或许这也是天命⋯⋯",
                 desc: "        很努力的你也难以把控风云变测的市场，可能是决策的失误，也可能是对手的强大，让你走在了濒临破产的路上。幸好公司清算后还剩了一点钱，算是对你这些年努力付出的慰藉。或许，你可以东山再起，重新尝试一遍？",
+                weixintitle:"创业那些年，马失前蹄",
                 imageTag: "fail_tag_png",
                 imageBg: "fail_bg_png"
             }
