33b4252cbed51d780d8e2c7f747efde7a3c17f01
SinCatGit <sincat@126.com>
2016-03-17 01:07:58 +0800

    添加开始页图片

--- src/BeginUI.ts
+++ src/BeginUI.ts
@@ -5,18 +5,35 @@
  */
 class BeginUI extends eui.Component {
     private beginBackgroundImg: eui.Image;
-    private beginBackgroundTitleImg: eui.Image;
+    private beginBackground1Img: eui.Image;
+    private beginBackgroundTitleImg: eui.Image;  
     private beginEnterImg: eui.Image;
+    private twTimer: egret.Timer;
+//    private titleTimer: egret.Timer;
 	public constructor() {
     	super();
     	this.skinName = "resource/custom_eui_skins/beginUISkin.exml";
-    	var tw = egret.Tween.get(this.beginBackgroundTitleImg);
-    	tw.to({alpha:1, y:80},1000);
+      this.twTimer = new egret.Timer(700,0);
+      this.twTimer.addEventListener(egret.TimerEvent.TIMER,this.twTimerFunc,this);
+      this.twTimer.start();
+      
+//      this.titleTimer = new egret.Timer(500,0);
+//      this.titleTimer.addEventListener(egret.TimerEvent.TIMER,this.titleTimerFunc,this);
+//      this.titleTimer.start();
     	var enterTw = egret.Tween.get(this.beginEnterImg);
-    	enterTw.to({alpha:1, width:190, height:73},500);
+    	enterTw.to({alpha:1, width:195, height:70},500);
     	
     	this.beginEnterImg.addEventListener(egret.TouchEvent.TOUCH_TAP,()=>{
         	this.dispatchEventWith(GameEvents.EVT_BEGIN);
         	},this);
 	}
+	
+    private twTimerFunc(event: egret.TimerEvent) {
+        this.beginBackground1Img.visible = !this.beginBackground1Img.visible;
+        this.beginBackgroundTitleImg.visible = !this.beginBackgroundTitleImg.visible;
+    }
+    
+//    private titleTimerFunc(event: egret.TimerEvent) {
+//        this.beginBackgroundTitleImg.visible = !this.beginBackgroundTitleImg.visible;
+//    }
 }
